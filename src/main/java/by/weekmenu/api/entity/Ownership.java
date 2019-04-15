58cf27e3b5538d0441ffa67e4e932b064cc92e4d
Shurferer <40690225+Shurferer@users.noreply.github.com>
2019-04-15 21:19:53 +0300

    Added right side to the test branch

---
+++ src/main/java/by/weekmenu/api/entity/Ownership.java
@@ -0,0 +1,32 @@
+package by.weekmenu.api.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import lombok.EqualsAndHashCode;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = {"id"})
+@Entity
+@Table (name = "OWNERSHIP")
+public class Ownership implements Serializable {
+
+    private static final long serialVersionUID = 1002642071168789374L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column (name = "NAME", unique = true)
+    private String name;
+
+    public Ownership(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/by/weekmenu/api/entity/UnitOfMeasure.java
@@ -0,0 +1,32 @@
+package by.weekmenu.api.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import lombok.EqualsAndHashCode;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = {"id"})
+@Entity
+@Table (name = "UNIT_OF_MEASURE")
+public class UnitOfMeasure implements Serializable {
+
+    private static final long serialVersionUID = 1000642071168789374L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NAME", unique = true)
+    private String name;
+
+    public UnitOfMeasure(String name) {
+        this.name = name;
+    }
+}
