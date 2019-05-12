090fb0469a9fbc04c11a6fb4aec3bcb60fb7a07a
AlexandrRogov <rohaualexandr@gmail.com>
2019-05-13 01:18:59 +0300

    Revert: Merge branch 'feature/WMD-70--Change-Ingredient_Currency-to-Ingredient_Price' into dev

--- src/test/java/by/weekmenu/api/entity/IngredientPriceTest.java
+++ src/test/java/by/weekmenu/api/entity/IngredientPriceTest.java
@@ -107,7 +107,7 @@ public class IngredientPriceTest {
     }
 
     @Test
-    public void testIngredientPriceIsValid() {
+    public void testIngredientCurrencyIsValid() {
         IngredientPrice ingredientPrice = new IngredientPrice(new BigDecimal("111"),
                 new Ingredient("курица", new Ownership("пользователь"), new UnitOfMeasure("литр")),
                 getRegion());
