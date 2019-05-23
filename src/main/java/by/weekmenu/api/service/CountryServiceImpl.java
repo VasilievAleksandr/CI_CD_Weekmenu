91496f5dae9f4e761648b039b2a2b389eabfcdef
Katerina <katerinalupacheva@gmail.com>
2019-05-23 17:38:52 +0300

    CountryServiceImpl added

---
+++ src/main/java/by/weekmenu/api/service/CountryServiceImpl.java
@@ -0,0 +1,61 @@
+package by.weekmenu.api.service;
+
+import by.weekmenu.api.dto.CountryDto;
+import by.weekmenu.api.entity.Country;
+import by.weekmenu.api.entity.Currency;
+import by.weekmenu.api.repository.CountryRepository;
+import by.weekmenu.api.repository.CurrencyRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class CountryServiceImpl implements CrudService<CountryDto, Long> {
+
+    private final CountryRepository countryRepository;
+    private final CurrencyRepository currencyRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public CountryDto save(CountryDto entityDto) {
+        return convertToDto(countryRepository.save(convertToEntity(entityDto)));
+    }
+
+    @Override
+    public CountryDto findById(Long id) {
+        return convertToDto(countryRepository.findById(id).orElse(null));
+    }
+
+    @Override
+    public void delete(Long id) {
+        countryRepository.deleteById(id);
+    }
+
+    @Override
+    public List<CountryDto> findAll() {
+        return StreamSupport.stream(countryRepository.findAll().spliterator(), false)
+                .filter(Objects::nonNull)
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    private Country convertToEntity(CountryDto countryDto) {
+        Country country = modelMapper.map(countryDto, Country.class);
+        Currency currency = currencyRepository.findAllByCode(countryDto.getCurrencyCode());
+        if (currency!=null) {
+            country.setCurrency(currency);
+        }
+        return country;
+    }
+
+    private CountryDto convertToDto(Country country) {
+        return modelMapper.map(country, CountryDto.class);
+    }
+}
