74a54981c2aed90382d9ed853fc6563b6d264943
Shurferer <40690225+Shurferer@users.noreply.github.com>
2019-06-02 22:49:53 +0300

    Currency Controller/Repository/Service changed

--- src/main/java/by/weekmenu/api/service/CurrencyService.java
+++ src/main/java/by/weekmenu/api/service/CurrencyService.java
@@ -1,9 +1,16 @@
 package by.weekmenu.api.service;
 
+import by.weekmenu.api.entity.Currency;
+
 import java.util.List;
 
 public interface CurrencyService {
 
     List<String> getAllCurrencyCodes();
 
+    Currency findByName(String name);
+
+    Currency findByCode(String code);
+
+    Currency findBySymbol(String symbol);
 }
