e06ff1d1512ab10a4c1baecac58fd8050e2fc68b
Katerina <katerinalupacheva@gmail.com>
2019-06-24 10:50:33 +0300

    Bidirectional relations removed from Region

--- src/main/java/by/weekmenu/api/entity/Region.java
+++ src/main/java/by/weekmenu/api/entity/Region.java
@@ -11,14 +11,12 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
 
 
 @NoArgsConstructor
 @Getter
 @Setter
-@EqualsAndHashCode(exclude = {"id", "recipePrices", "dailyMenuStatisticsPrices"})
+@EqualsAndHashCode(exclude = {"id"})
 @Entity
 @Table(name = "REGION")
 public class Region implements Serializable {
@@ -38,30 +36,10 @@ public class Region implements Serializable {
     private String name;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "COUNTRY_ID",
-            updatable = false,
-            insertable = false)
+    @JoinColumn(name = "COUNTRY_ID")
     @Valid
     @NotNull(message = "Region's Country mustn't be null.")
     private Country country;
-
-    @OneToMany(mappedBy = "region", cascade = CascadeType.PERSIST)
-    private Set<
-            @Valid
-            @NotNull(message = "Region must have list of recipePrices without null elements.")
-                    RecipePrice> recipePrices = new HashSet<>();
-
-    @OneToMany(mappedBy = "region", cascade = CascadeType.PERSIST)
-    private Set<
-            @Valid
-            @NotNull(message = "Region must have list of dailyMenuStatisticsPrices without null elements.")
-                    DailyMenuStatisticsPrice> dailyMenuStatisticsPrices = new HashSet<>();
-
-    @OneToMany(mappedBy = "region", cascade = CascadeType.PERSIST)
-    private Set<
-            @Valid
-            @NotNull(message = "Region must have list of ingredientPrices without null elements.")
-                    IngredientPrice> ingredientPrices = new HashSet<>();
 
     public Region(String name, Country country) {
         this.name = name;
