c35ea7e9c30a32ef287d74657f62ff3eb3d4b9db
Katerina <katerinalupacheva@gmail.com>
2019-06-24 10:47:48 +0300

    IngredientPrices CRUD added to IngredientServiceImpl

--- src/main/java/by/weekmenu/api/service/IngredientServiceImpl.java
+++ src/main/java/by/weekmenu/api/service/IngredientServiceImpl.java
@@ -1,14 +1,9 @@
 package by.weekmenu.api.service;
 
 import by.weekmenu.api.dto.IngredientDto;
-import by.weekmenu.api.entity.Ingredient;
-import by.weekmenu.api.entity.IngredientUnitOfMeasure;
-import by.weekmenu.api.entity.OwnershipName;
-import by.weekmenu.api.entity.UnitOfMeasure;
-import by.weekmenu.api.repository.IngredientRepository;
-import by.weekmenu.api.repository.IngredientUnitOfMeasureRepository;
-import by.weekmenu.api.repository.OwnershipRepository;
-import by.weekmenu.api.repository.UnitOfMeasureRepository;
+import by.weekmenu.api.dto.IngredientPriceDTO;
+import by.weekmenu.api.entity.*;
+import by.weekmenu.api.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +23,8 @@ public class IngredientServiceImpl implements IngredientService {
     private final OwnershipRepository ownershipRepository;
     private final UnitOfMeasureRepository unitOfMeasureRepository;
     private final IngredientUnitOfMeasureRepository ingredientUnitOfMeasureRepository;
+    private final RegionRepository regionRepository;
+    private final IngredientPriceRepository ingredientPriceRepository;
     private final ModelMapper modelMapper;
 
     @Override
@@ -40,18 +37,41 @@ public class IngredientServiceImpl implements IngredientService {
         ownershipRepository.findByName(OwnershipName.ADMIN).ifPresent(ingredient::setOwnership);
         ingredientRepository.save(ingredient);
         if (entityDto.getUnitOfMeasureEquivalent()!=null) {
-            Map<String, BigDecimal> map = entityDto.getUnitOfMeasureEquivalent();
-            map.forEach((k, v) -> {
-                UnitOfMeasure unitOfMeasure = unitOfMeasureRepository.findByFullNameIgnoreCase(k).orElse(null);
-                IngredientUnitOfMeasure ingredientUnitOfMeasure = new IngredientUnitOfMeasure();
-                ingredientUnitOfMeasure.setId(new IngredientUnitOfMeasure.Id(ingredient.getId(), unitOfMeasure.getId()));
-                ingredientUnitOfMeasure.setIngredient(ingredient);
-                ingredientUnitOfMeasure.setUnitOfMeasure(unitOfMeasure);
-                ingredientUnitOfMeasure.setEquivalent(v);
-                ingredientUnitOfMeasureRepository.save(ingredientUnitOfMeasure);
-            });
+            saveIngredientUOM(entityDto, ingredient);
+        }
+        if (entityDto.getIngredientPrices()!=null) {
+            saveIngredientPrice(entityDto, ingredient);
         }
         return convertToDto(ingredient);
+    }
+
+    private void saveIngredientPrice(IngredientDto entityDto, Ingredient ingredient) {
+        Set<IngredientPriceDTO> ingredientPricesDTO = entityDto.getIngredientPrices();
+        ingredientPricesDTO.forEach(ingredientPriceDTO -> {
+            IngredientPrice ingredientPrice = new IngredientPrice();
+            Region region = regionRepository.findByNameIgnoreCase(ingredientPriceDTO.getRegionName()).orElse(null);
+            ingredientPrice.setId(new IngredientPrice.Id(ingredient.getId(), Objects.requireNonNull(region).getId()));
+            UnitOfMeasure unitOfMeasure = unitOfMeasureRepository.findByFullNameIgnoreCase(ingredientPriceDTO.getUnitOfMeasureName()).orElse(null);
+            ingredientPrice.setUnitOfMeasure(unitOfMeasure);
+            ingredientPrice.setIngredient(ingredient);
+            ingredientPrice.setRegion(region);
+            ingredientPrice.setPriceValue(ingredientPriceDTO.getPriceValue());
+            ingredientPrice.setQuantity(ingredientPriceDTO.getQuantity());
+            ingredientPriceRepository.save(ingredientPrice);
+        });
+    }
+
+    private void saveIngredientUOM(IngredientDto entityDto, Ingredient ingredient) {
+        Map<String, BigDecimal> map = entityDto.getUnitOfMeasureEquivalent();
+        map.forEach((k, v) -> {
+            UnitOfMeasure unitOfMeasure = unitOfMeasureRepository.findByFullNameIgnoreCase(k).orElse(null);
+            IngredientUnitOfMeasure ingredientUnitOfMeasure = new IngredientUnitOfMeasure();
+            ingredientUnitOfMeasure.setId(new IngredientUnitOfMeasure.Id(ingredient.getId(), Objects.requireNonNull(unitOfMeasure).getId()));
+            ingredientUnitOfMeasure.setIngredient(ingredient);
+            ingredientUnitOfMeasure.setUnitOfMeasure(unitOfMeasure);
+            ingredientUnitOfMeasure.setEquivalent(v);
+            ingredientUnitOfMeasureRepository.save(ingredientUnitOfMeasure);
+        });
     }
 
     @Override
@@ -82,9 +102,25 @@ public class IngredientServiceImpl implements IngredientService {
         IngredientDto ingredientDto = modelMapper.map(ingredient, IngredientDto.class);
         List<IngredientUnitOfMeasure> ingredientUOMlist = ingredientUnitOfMeasureRepository.findAllById_IngredientId(ingredient.getId());
         Map<String, BigDecimal> map = new HashMap<>();
-        ingredientUOMlist.forEach(ingredientUnitOfMeasure -> map.put(unitOfMeasureRepository.findById(ingredientUnitOfMeasure.getUnitOfMeasure().getId()).orElse(null).getFullName(),
+        ingredientUOMlist.forEach(ingredientUnitOfMeasure ->
+                map.put(Objects.requireNonNull(unitOfMeasureRepository.findById(ingredientUnitOfMeasure.getUnitOfMeasure().getId())
+                                .orElse(null)).getFullName(),
                 ingredientUnitOfMeasure.getEquivalent()));
         ingredientDto.setUnitOfMeasureEquivalent(map);
+
+        Set<IngredientPriceDTO> ingredientPriceDTOS = new HashSet<>();
+        Set<IngredientPrice> ingredientPrices = ingredientPriceRepository.findAllById_IngredientId(ingredient.getId());
+        for(IngredientPrice tempIngredientPrice : ingredientPrices) {
+            IngredientPriceDTO ingredientPriceDTO = new IngredientPriceDTO();
+            ingredientPriceDTO.setUnitOfMeasureName(tempIngredientPrice.getUnitOfMeasure().getFullName());
+            ingredientPriceDTO.setRegionName(tempIngredientPrice.getRegion().getName());
+            ingredientPriceDTO.setPriceValue(tempIngredientPrice.getPriceValue());
+            ingredientPriceDTO.setQuantity(tempIngredientPrice.getQuantity());
+            regionRepository.findByNameIgnoreCase(tempIngredientPrice.getRegion().getName())
+                    .ifPresent(region -> ingredientPriceDTO.setCurrencyCode(region.getCountry().getCurrency().getCode()));
+            ingredientPriceDTOS.add(ingredientPriceDTO);
+        }
+        ingredientDto.setIngredientPrices(ingredientPriceDTOS);
         return ingredientDto;
     }
 
