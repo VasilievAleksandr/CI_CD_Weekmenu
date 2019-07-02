e58861b8b2d47823d13a27dedfb3379f989efb30
AlexandrRogov <rohaualexandr@gmail.com>
2019-07-02 17:34:09 +0300

    add contact and Api from Region,Country,UnitOfMeasure

--- src/main/java/by/weekmenu/api/controller/UnitOfMeasureController.java
+++ src/main/java/by/weekmenu/api/controller/UnitOfMeasureController.java
@@ -1,9 +1,10 @@
 package by.weekmenu.api.controller;
 
 import by.weekmenu.api.dto.UnitOfMeasureDto;
-import by.weekmenu.api.entity.UnitOfMeasure;
 import by.weekmenu.api.service.CrudService;
 import by.weekmenu.api.service.UnitOfMeasureService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping({"/unitOfMeasures"})
+@Api(description = "REST API для сущности UnitOfMeasure")
 public class UnitOfMeasureController {
 
     private final CrudService<UnitOfMeasureDto, Long> crudService;
@@ -23,16 +25,19 @@ public class UnitOfMeasureController {
     }
 
     @GetMapping
+    @ApiOperation("Возвращает список всех UnitOfMeasure")
     public List<UnitOfMeasureDto> findAllUnitOfMeasure() {
         return crudService.findAll();
     }
 
     @GetMapping("/{id}")
+    @ApiOperation("Находит UnitOfMeasure по его Id")
     public UnitOfMeasureDto findUnitOfMeasureById(@PathVariable("id") Long id) {
         return crudService.findById(id);
     }
     
     @GetMapping("/checkUniqueShortName")
+    @ApiOperation("Проверяет поле shortName у UnitOfMeasure на уникальность. Возвращает -1, если поле есть в БД и 0, если нет.")
     public Integer checkUniqueShortName(@RequestParam String shortName) {
         if (unitOfMeasureService.findByShortName(shortName) != null) {
             return -1;
@@ -42,6 +47,7 @@ public class UnitOfMeasureController {
     }
 
     @GetMapping("/checkUniqueFullName")
+    @ApiOperation("Проверяет поле fullName у UnitOfMeasure на уникальность. Возвращает -1, если поле есть в БД и 0, если нет.")
     public Integer checkUniqueFullName(@RequestParam String fullName) {
         if (unitOfMeasureService.findByFullName(fullName) != null) {
             return -1;
@@ -51,11 +57,13 @@ public class UnitOfMeasureController {
     }
 
     @PostMapping
+    @ApiOperation("Сохраняет UnitOfMeasure.")
     public UnitOfMeasureDto addUnitOfMeasure(@RequestBody UnitOfMeasureDto unitOfMeasureDTO) {
         return crudService.save(unitOfMeasureDTO);
     }
 
     @PutMapping("/{id}")
+    @ApiOperation("Обновляет UnitOfMeasure по Id.")
     public UnitOfMeasureDto updateUnitOfMeasure(@RequestBody UnitOfMeasureDto unitOfMeasureDTO, @PathVariable("id") Long id) {
         UnitOfMeasureDto newUnitOfMeasureDto = crudService.findById(id);
         if (newUnitOfMeasureDto != null) {
@@ -66,6 +74,7 @@ public class UnitOfMeasureController {
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation("Удаляет UnitOfMeasure по Id.")
     public void deleteUnitOfMeasureById(@PathVariable("id") Long id) {
         UnitOfMeasureDto newUnitOfMeasureDto = crudService.findById(id);
         if (newUnitOfMeasureDto != null) crudService.delete(id);
