37e08187ace45db0b3a7292a0660168ed1a3b2ee
Katerina <katerinalupacheva@gmail.com>
2019-07-25 15:35:00 +0300

    IngredientUnitOfMeasureRepository and OwnershipRepository updated

--- src/main/java/by/weekmenu/api/repository/OwnershipRepository.java
+++ src/main/java/by/weekmenu/api/repository/OwnershipRepository.java
@@ -1,12 +1,11 @@
 package by.weekmenu.api.repository;
 
 import by.weekmenu.api.entity.Ownership;
-import by.weekmenu.api.entity.OwnershipName;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
 public interface OwnershipRepository extends CrudRepository<Ownership, Long> {
 
-    Optional<Ownership> findByName(OwnershipName name);
+    Optional<Ownership> findByName(String name);
 }
