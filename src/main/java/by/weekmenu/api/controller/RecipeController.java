9952bcd5536c4fe8159da0eb88980bf005a5c889
Katerina <katerinalupacheva@gmail.com>
2019-07-25 15:32:41 +0300

    RecipeController update (RecipeDTO renamed)

--- src/main/java/by/weekmenu/api/controller/RecipeController.java
+++ src/main/java/by/weekmenu/api/controller/RecipeController.java
@@ -1,6 +1,6 @@
 package by.weekmenu.api.controller;
 
-import by.weekmenu.api.dto.RecipeDto;
+import by.weekmenu.api.dto.RecipeDTO;
 import by.weekmenu.api.service.RecipeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -24,22 +24,22 @@ public class RecipeController {
 
     @GetMapping
     @ApiOperation("Возвращает список всех рецептов")
-    public ResponseEntity<List<RecipeDto>> findAllRecipes() {
+    public ResponseEntity<List<RecipeDTO>> findAllRecipes() {
         return new ResponseEntity<>(recipeService.findAll(), HttpStatus.OK);
     }
 
     @PostMapping
     @ApiOperation("Сохраняет рецепт")
-    public ResponseEntity<RecipeDto> addRecipe(@RequestBody RecipeDto recipeDto) {
+    public ResponseEntity<RecipeDTO> addRecipe(@RequestBody RecipeDTO recipeDto) {
         return new ResponseEntity<>(recipeService.save(recipeDto), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     @ApiOperation("Обновляет рецепт по Id")
-    public ResponseEntity<RecipeDto> updateRecipe(@RequestBody RecipeDto updatedRecipeDto, @PathVariable ("id") Long id) {
-        RecipeDto recipeDto = recipeService.findById(id);
+    public ResponseEntity<RecipeDTO> updateRecipe(@RequestBody RecipeDTO updatedRecipeDTO, @PathVariable ("id") Long id) {
+        RecipeDTO recipeDto = recipeService.findById(id);
         if (recipeDto!=null) {
-            return new ResponseEntity<>(recipeService.save(modelMapper.map(updatedRecipeDto, RecipeDto.class)), HttpStatus.OK);
+            return new ResponseEntity<>(recipeService.save(modelMapper.map(updatedRecipeDTO, RecipeDTO.class)), HttpStatus.OK);
         } else {
             return new ResponseEntity<>( HttpStatus.NOT_FOUND);
         }
@@ -48,7 +48,7 @@ public class RecipeController {
     @DeleteMapping("/{id}")
     @ApiOperation("Удаляет рецепт по Id")
     public ResponseEntity<Void> deleteRecipe(@PathVariable("id") Long id) {
-        RecipeDto recipeDto = recipeService.findById(id);
+        RecipeDTO recipeDto = recipeService.findById(id);
         if (recipeDto!=null) {
             recipeService.delete(id);
             return ResponseEntity.noContent().build();
