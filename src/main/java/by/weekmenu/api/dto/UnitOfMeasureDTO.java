fe079dd7416acbe55f8c00689a48b153dc58dbd5
Katerina <katerinalupacheva@gmail.com>
2019-08-01 15:00:55 +0300

    isArchived field added to UnitOfMeasure

---
+++ src/main/java/by/weekmenu/api/dto/UnitOfMeasureDTO.java
@@ -0,0 +1,14 @@
+package by.weekmenu.api.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UnitOfMeasureDTO {
+
+    private Long id;
+    private String fullName;
+    private String shortName;
+    private boolean isArchived;
+}
