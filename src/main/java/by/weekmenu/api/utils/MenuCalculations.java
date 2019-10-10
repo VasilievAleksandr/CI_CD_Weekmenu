947d860e1924c9d1ca325a4a33129e7a4140b27b
Katerina <katerinalupacheva@gmail.com>
2019-10-10 09:08:42 +0300

    Calculate daily average calories, proteins, fats and carbs for menu

--- src/main/java/by/weekmenu/api/utils/MenuCalculations.java
+++ src/main/java/by/weekmenu/api/utils/MenuCalculations.java
@@ -4,6 +4,7 @@ import by.weekmenu.api.dto.MenuDTO;
 import by.weekmenu.api.dto.MenuRecipeDTO;
 import by.weekmenu.api.entity.Menu;
 import by.weekmenu.api.entity.Recipe;
+import by.weekmenu.api.repository.MenuRecipeRepository;
 import by.weekmenu.api.repository.RecipeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,38 +12,45 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.DayOfWeek;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class MenuCalculations {
 
     private final RecipeRepository recipeRepository;
+    private final MenuRecipeRepository menuRecipeRepository;
 
     public void calculateCPFC(MenuDTO menuDTO, Menu menu) {
         Set<MenuRecipeDTO> menuRecipeDTOS = menuDTO.getMenuRecipeDTOS();
         BigDecimal totalCalories = BigDecimal.ZERO;
-        BigDecimal totalCarbs= BigDecimal.ZERO;
-        BigDecimal totalFats= BigDecimal.ZERO;
-        BigDecimal totalProteins= BigDecimal.ZERO;
+        BigDecimal totalCarbs = BigDecimal.ZERO;
+        BigDecimal totalFats = BigDecimal.ZERO;
+        BigDecimal totalProteins = BigDecimal.ZERO;
+
+        //get all days of week, when there are recipes in menu
+        Set<DayOfWeek> daysOfWeek = menuRecipeDTOS.stream()
+                .map(MenuRecipeDTO::getDayOfWeek).collect(Collectors.toSet());
+        BigDecimal daysCount = BigDecimal.valueOf(daysOfWeek.size());
+
+        //sum of calories, proteins, fats and carbs from each recipe which is present in menu
         for (MenuRecipeDTO menuRecipeDTO : menuRecipeDTOS) {
             Optional<Recipe> recipe = recipeRepository.findByNameIgnoreCase(menuRecipeDTO.getRecipeName());
             if (recipe.isPresent()) {
-                totalCalories = totalCalories.add(recipe.get().getCalories())
-                        .setScale(1, BigDecimal.ROUND_HALF_UP);
-                totalProteins = totalProteins.add(recipe.get().getProteins())
-                        .setScale(1, BigDecimal.ROUND_HALF_UP);
-                totalFats = totalFats.add(recipe.get().getFats())
-                        .setScale(1, BigDecimal.ROUND_HALF_UP);
-                totalCarbs = totalCarbs.add(recipe.get().getCarbs())
-                        .setScale(1, BigDecimal.ROUND_HALF_UP);
+                totalCalories = totalCalories.add(recipe.get().getCalories());
+                totalProteins = totalProteins.add(recipe.get().getProteins());
+                totalFats = totalFats.add(recipe.get().getFats());
+                totalCarbs = totalCarbs.add(recipe.get().getCarbs());
             }
         }
-        BigDecimal recipesCount = new BigDecimal(menuRecipeDTOS.size());
-        menu.setCalories(totalCalories.divide(recipesCount, 1, RoundingMode.HALF_UP));
-        menu.setProteins(totalProteins.divide(recipesCount, 1, RoundingMode.HALF_UP));
-        menu.setFats(totalFats.divide(recipesCount, 1, RoundingMode.HALF_UP));
-        menu.setCarbs(totalCarbs.divide(recipesCount, 1, RoundingMode.HALF_UP));
+
+        //daily average sum of calories, proteins, fats and carbs
+        menu.setCalories(totalCalories.divide(daysCount, 1, RoundingMode.HALF_UP));
+        menu.setProteins(totalProteins.divide(daysCount, 1, RoundingMode.HALF_UP));
+        menu.setFats(totalFats.divide(daysCount, 1, RoundingMode.HALF_UP));
+        menu.setCarbs(totalCarbs.divide(daysCount, 1, RoundingMode.HALF_UP));
     }
 }
